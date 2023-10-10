07ab5dba7769f6388f58bf2c8effe8e8bb9449c4
Gonzagarcia98 <138820538+Gonzagarcia98@users.noreply.github.com>
2023-10-10 17:07:34 -0300

    Add files via upload

--- Liquidaciondesueldos.py
+++ Liquidaciondesueldos.py
@@ -18,6 +18,12 @@ with col3 :
     st.write("")
 st.write("---")
 
+#Checkbox
+st.write("Usted es/será")
+empleado = st.checkbox("Empleado")
+empleador = st.checkbox("Empleador")
+
+
 
 #Input inicial 
 Sueldo_bruto = st.text_input('Indique el sueldo bruto', value="$")
@@ -37,49 +43,50 @@ else:
         aux3 = False        
         st.markdown("<span style='color: red;'>Ingrese un monto válido porfavor.</span>", unsafe_allow_html=True)
 st.write("---")
-
 #Aportes empleado 
 #Definimos variables y calculos
-if aux3 == True:
-    Aporte_jubilatorio = (0.11*Sueldo_bruto)
-    Obra_social = (0.06*Sueldo_bruto)
-    Pami = (0.03*Sueldo_bruto)
-    FAECyS = (0.005*Sueldo_bruto)
-    Sindicato = (0.02*Sueldo_bruto)
-    #Calculamos el sueldo neto luego de aportes
-    Sueldo_neto = Sueldo_bruto-(Aporte_jubilatorio+Obra_social+Pami+FAECyS+Sindicato)
+if st.button("Calcular"):
+    if aux3 == True:
 
-    #Cargas sociales empleador
-    #Definimos variables y calculos
-    Jubilacion = 0.16*Sueldo_bruto
-    Obra_social_empleador = 0.03*Sueldo_bruto
-    Pami_empleador = 0.019*Sueldo_bruto
-    Anses = 0.047*Sueldo_bruto
-    Fne = 0.0094*Sueldo_bruto
-    Seguro_vida = 0.003*Sueldo_bruto
+        Aporte_jubilatorio = (0.11*Sueldo_bruto)
+        Obra_social = (0.06*Sueldo_bruto)
+        Pami = (0.03*Sueldo_bruto)
+        FAECyS = (0.005*Sueldo_bruto)
+        Sindicato = (0.02*Sueldo_bruto)
+        #Calculamos el sueldo neto luego de aportes
+        Sueldo_neto = Sueldo_bruto-(Aporte_jubilatorio+Obra_social+Pami+FAECyS+Sindicato)
 
-    #Calculamos el total de cargas sociales
-    Cargas_sociales = Jubilacion+Obra_social_empleador+Pami_empleador+Anses+Fne+Seguro_vida
-    Cargas_sociales
+        #Cargas sociales empleador
+        #Definimos variables y calculos
+        Jubilacion = 0.16*Sueldo_bruto
+        Obra_social_empleador = 0.03*Sueldo_bruto
+        Pami_empleador = 0.019*Sueldo_bruto
+        Anses = 0.047*Sueldo_bruto
+        Fne = 0.0094*Sueldo_bruto
+        Seguro_vida = 0.003*Sueldo_bruto
 
-
-    lista_variables = [Aporte_jubilatorio, Obra_social,Pami,FAECyS,Sindicato,Jubilacion,Obra_social_empleador,Pami_empleador,Anses,Fne,Seguro_vida,Cargas_sociales,Sueldo_neto]
-    for i in range (len(lista_variables)) :
-        lista_variables[i] = '{:,.2f}'.format(lista_variables[i]).replace(',', ' ')
-        lista_variables[i] = lista_variables[i].replace(".",",")
-        lista_variables[i] = lista_variables[i].replace(" ",".")
-    print(f"El sueldo neto a cobrar luego del descuento de los aportes es de: ${lista_variables[12]}")
-    print(f"Los aportes que realiza el empleador en concepto de cargas sociales son de: ${lista_variables[11]}")
+        #Calculamos el total de cargas sociales
+        Cargas_sociales = Jubilacion+Obra_social_empleador+Pami_empleador+Anses+Fne+Seguro_vida
+        Cargas_sociales
 
 
-    pdf_filename = "Liquidación de sueldos 2.pdf"
-    c = canvas.Canvas(pdf_filename, pagesize=letter)
-    texto = f"Los aportes que realiza el empleador en concepto de cargas sociales son de: ${lista_variables[11]}"
-    texto2 = f"El sueldo neto a cobrar luego del descuento de los aportes es de: ${lista_variables[12]}"
-    c.drawString(90, 750, texto)
-    c.drawString(90, 700, texto2)
-    # Agregar una imagen al PDF
-    imagen_filename = r"imgs/logos_came_recortados.png"  # Reemplaza con el nombre de tu imagen
-    c.drawImage(imagen_filename, 100, 500, width=200, height=100)
-    c.save()
-    print(f"Se ha creado el archivo PDF: {pdf_filename}")
+        lista_variables = [Aporte_jubilatorio, Obra_social,Pami,FAECyS,Sindicato,Jubilacion,Obra_social_empleador,Pami_empleador,Anses,Fne,Seguro_vida,Cargas_sociales,Sueldo_neto]
+        for i in range (len(lista_variables)) :
+            lista_variables[i] = '{:,.2f}'.format(lista_variables[i]).replace(',', ' ')
+            lista_variables[i] = lista_variables[i].replace(".",",")
+            lista_variables[i] = lista_variables[i].replace(" ",".")
+        print(f"El sueldo neto a cobrar luego del descuento de los aportes es de: ${lista_variables[12]}")
+        print(f"Los aportes que realiza el empleador en concepto de cargas sociales son de: ${lista_variables[11]}")
+
+
+        pdf_filename = "Liquidación de sueldos 2.pdf"
+        c = canvas.Canvas(pdf_filename, pagesize=letter)
+        texto = f"Los aportes que realiza el empleador en concepto de cargas sociales son de: ${lista_variables[11]}"
+        texto2 = f"El sueldo neto a cobrar luego del descuento de los aportes es de: ${lista_variables[12]}"
+        c.drawString(90, 750, texto)
+        c.drawString(90, 700, texto2)
+        # Agregar una imagen al PDF
+        imagen_filename = r"imgs/logos_came_recortados.png"  # Reemplaza con el nombre de tu imagen
+        c.drawImage(imagen_filename, 100, 500, width=200, height=100)
+        c.save()
+        print(f"Se ha creado el archivo PDF: {pdf_filename}")
