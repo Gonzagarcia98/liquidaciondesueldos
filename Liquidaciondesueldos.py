516c18fdc7d3b8a044b5b74ff29d23e57df9c9d1
Gonzagarcia98 <138820538+Gonzagarcia98@users.noreply.github.com>
2023-10-11 11:17:45 -0300

    Add files via upload

--- Liquidaciondesueldos.py
+++ Liquidaciondesueldos.py
@@ -3,12 +3,12 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import streamlit as st
 
-#Titulo
+#TITULO
 st.markdown("<h1 style='text-align: center; font-size: 54px; font-family: Verdana, sans-serif;'>Calculadora Sueldos</h1>", unsafe_allow_html=True)
 st.write("---")
 
 
-#Imagenes
+#IMAGEENES
 col1, col2, col3 = st.columns([0.5,3,0.5])
 with col1 :
     st.write("")
@@ -18,12 +18,12 @@ with col3 :
     st.write("")
 st.write("---")
 
-#Input inicial 
+#INPUT INICIAL
 Sueldo_bruto = st.text_input('Indique el sueldo bruto', value="$")
 Sueldo_bruto = Sueldo_bruto.strip()
 Sueldo_bruto = Sueldo_bruto.replace("$", "").replace(".","").replace(",,",",").replace(",",".")
 
-#Manejo de errores - Input
+#MANEJO DE ERRORES DEL INPUT
 if Sueldo_bruto == "" or Sueldo_bruto == "$" or Sueldo_bruto == " " : 
     aux3 = False 
 elif Sueldo_bruto == "0":
@@ -38,9 +38,8 @@ else:
         st.markdown("<span style='color: red;'>Ingrese un monto válido porfavor.</span>", unsafe_allow_html=True)
 st.write("---")
 
-
-st.write("***Usted es/será***")
-opcion_seleccionada = st.radio("Selecciona una opción:", ["Empleado", "Empleador"], horizontal=True)
+#SELECCIONAMOS EMPLEADO O EMPLEADOR
+opcion_seleccionada = st.radio("***Usted es/será***", ["Empleado", "Empleador"], horizontal=True)
 if opcion_seleccionada == "Empleado":
     st.write("Seleccionaste Empleado")
 elif opcion_seleccionada == "Empleador":
@@ -48,9 +47,8 @@ elif opcion_seleccionada == "Empleador":
 else:
     st.write("No seleccionaste ninguna opción")
 
-
-#Aportes empleado 
-#Definimos variables y calculos
+#DEFINIMOS VARIABLES Y CALCULOS
+#APORTES EMPLEADO
 if st.button("Calcular"):
     if aux3 == True:
 
@@ -59,11 +57,11 @@ if st.button("Calcular"):
         Pami = (0.03*Sueldo_bruto)
         FAECyS = (0.005*Sueldo_bruto)
         Sindicato = (0.02*Sueldo_bruto)
-        #Calculamos el sueldo neto luego de aportes
+#CALCULAMOS EL NETO LUEGO DE APORTES
         Sueldo_neto = Sueldo_bruto-(Aporte_jubilatorio+Obra_social+Pami+FAECyS+Sindicato)
 
-        #Cargas sociales empleador
-        #Definimos variables y calculos
+#CARGAS SOCIALES DEL EMPLEADOR
+#DEFINIMOS VARIABLES DEL EMPLEADOR
         Jubilacion = 0.16*Sueldo_bruto
         Obra_social_empleador = 0.03*Sueldo_bruto
         Pami_empleador = 0.019*Sueldo_bruto
@@ -71,11 +69,11 @@ if st.button("Calcular"):
         Fne = 0.0094*Sueldo_bruto
         Seguro_vida = 0.003*Sueldo_bruto
 
-        #Calculamos el total de cargas sociales
+#CALCULAMOS EL TOTAL DE CARGAS SOCIALES
         Cargas_sociales = Jubilacion+Obra_social_empleador+Pami_empleador+Anses+Fne+Seguro_vida
         Cargas_sociales
 
-
+#REEEMPLAZAMS LOS . DE LOS MILES
         lista_variables = [Aporte_jubilatorio, Obra_social,Pami,FAECyS,Sindicato,Jubilacion,Obra_social_empleador,Pami_empleador,Anses,Fne,Seguro_vida,Cargas_sociales,Sueldo_neto]
         for i in range (len(lista_variables)) :
             lista_variables[i] = '{:,.2f}'.format(lista_variables[i]).replace(',', ' ')
@@ -84,7 +82,7 @@ if st.button("Calcular"):
         print(f"El sueldo neto a cobrar luego del descuento de los aportes es de: ${lista_variables[12]}")
         print(f"Los aportes que realiza el empleador en concepto de cargas sociales son de: ${lista_variables[11]}")
 
-
+#DESCARGAMOS EL PDF
         pdf_filename = "Liquidación de sueldos 2.pdf"
         c = canvas.Canvas(pdf_filename, pagesize=letter)
         texto = f"Los aportes que realiza el empleador en concepto de cargas sociales son de: ${lista_variables[11]}"
