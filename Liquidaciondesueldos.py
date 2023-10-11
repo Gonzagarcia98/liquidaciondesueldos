1008a49c72b43df45b1cf5cc7adc2df10599f0a9
Gonzagarcia98 <138820538+Gonzagarcia98@users.noreply.github.com>
2023-10-11 12:42:43 -0300

    Add files via upload

--- Liquidaciondesueldos.py
+++ Liquidaciondesueldos.py
@@ -47,6 +47,7 @@ st.write("---")
 #APORTES EMPLEADO
 colA, colB= st.columns(2)
 aux1 = False
+aux2= False
 with colA:
     if st.button("Calcular"):
         if aux3 == True:
@@ -109,8 +110,8 @@ with colA:
                         lista_variables[i] = '{:,.2f}'.format(lista_variables[i]).replace(',', ' ')
                         lista_variables[i] = lista_variables[i].replace(".",",")
                         lista_variables[i] = lista_variables[i].replace(" ",".")
+                aux2 = True
                         
-                st.write(f"Los aportes que realiza el empleador en concepto de cargas sociales son de: ${lista_variables[6]}")
                 pdf_buffer = BytesIO()
                     # Generar el PDF
                 c = canvas.Canvas(pdf_buffer, pagesize=letter)
@@ -148,10 +149,42 @@ with colB:
             else:
                 st.write("")    
 st.write("---") 
-if aux1 == True:      
+if aux2 == True:      
         st.write("***DETALLE DE DESCUENTOS***")
         st.write(f"Aporte jubilatorio: {lista_variables[0]}")
         st.write(f"Obra social: {lista_variables[1]}")
         st.write(f"Pami: {lista_variables[2]}")
         st.write(F"Faecys: {lista_variables[3]}")
         st.write(f"Sindicato: {lista_variables[4]}")
+with colB:
+            custom_css = """
+        <style>
+            .tarjeta {
+                text-align: left;
+            }
+            .subheader {
+                font-size: 20px;
+                font-weight: bold;
+            }
+        </style>
+        """
+        # Agregar el estilo CSS personalizado utilizando st.markdown      
+            if aux2 == True :
+                st.markdown(custom_css, unsafe_allow_html=True)
+                tarjeta = f'<div class="tarjeta" style="font-size: 45px;font-weight: bold; ">${lista_variables[6]}</div>'
+                st.markdown('<div class="subheader">El total de cargas sociales será:</div>', unsafe_allow_html=True)
+                st.markdown(tarjeta, unsafe_allow_html=True)
+                st.markdown('</div></div>', unsafe_allow_html=True)
+                #st.write(f"El precio sugerido es:")
+                #st.write(f"# $**{monto_final}**")
+            else:
+                st.write("") 
+st.write("---") 
+if aux2 == True:      
+        st.write("***DETALLE DE CARGAS SOCIALES***")
+        st.write(f"Jubilación: {lista_variables[0]}")
+        st.write(f"Obra social: {lista_variables[1]}")
+        st.write(f"Pami: {lista_variables[2]}")
+        st.write(F"Anses: {lista_variables[3]}")
+        st.write(F"FNE: {lista_variables[4]}")
+        st.write(F"Seguro de vida: {lista_variables[5]}") 
